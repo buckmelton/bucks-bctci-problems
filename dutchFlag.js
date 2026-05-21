b5124d8fc416b413cf889e36f5341ce3d490fc5f
Buck <buckm@alumni.stanford.edu>
2026-05-21 09:36:21 -0700

    Add Dutch Flag sorting algorithm
    Implement Dutch National Flag algorithm to sort colors.

--- dutchFlag.js
+++ dutchFlag.js
@@ -14,3 +14,37 @@
 
 // - 0 ≤ arr.length ≤ 10^6
 // - arr[i] is either 'R', 'W', or 'B'
+
+// Idea: use frequency counts to count up how many red, white and blue elements
+// there are, then go back through array and simply overwrite array with the correct
+// number of red, then white, then blue elements
+function dutchFlag(arr) {
+  const freqs = { R : 0,
+                 W : 0,
+                 B : 0
+               };
+  for (let elt of arr) {
+    freqs[elt]++;
+  }
+  let i = 0;
+  while (i < freqs['R']) {
+    arr[i] = 'R';
+    i++;
+  }
+  let whiteEnd = freqs['R'] + freqs['W'];
+  while (i < whiteEnd) {
+    arr[i] = 'W';
+    i++;
+  }
+  while (i < arr.length) {
+    arr[i] = 'B';
+    i++;
+  }
+  return arr;
+}
+
+console.log(dutchFlag(['R', 'W', 'B', 'B', 'W', 'R', 'W']));
+console.log(dutchFlag(['B', 'R']));
+console.log(dutchFlag([]));
+console.log(dutchFlag(['R', 'R', 'R', 'R']));
+console.log(dutchFlag(['B', 'W', 'R']));
