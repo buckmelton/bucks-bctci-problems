55e89a7284edd921ed8af41f8a501194ba898067
Buck <buckm@alumni.stanford.edu>
2026-05-21 09:33:31 -0700

    Description of Dutch Flag Problem with examples and constraints

---
+++ dutchFlag.js
@@ -0,0 +1,16 @@
+// # Dutch Flag Problem
+
+// Given an array, `arr`, containing only of the characters 'R' (red), 'W' (white), and 'B' (blue), sort the array in place so that the same colors are adjacent, with the colors in the order red, white, and blue.
+
+// Example 1:
+// Input: arr = ['R', 'W', 'B', 'B', 'W', 'R', 'W']
+// Output: ['R', 'R', 'W', 'W', 'W', 'B', 'B']
+
+// Example 2:
+// Input: arr = ['B', 'R']
+// Output: ['R', 'B']
+
+// Constraints:
+
+// - 0 ≤ arr.length ≤ 10^6
+// - arr[i] is either 'R', 'W', or 'B'
