fa41dd0899cd325e164a7cea926f27900d714961
Buck <buckm@alumni.stanford.edu>
2026-06-01 14:18:54 -0700

    Add array intersection description

---
+++ 27-11-array-intersection.js
@@ -0,0 +1,31 @@
+// # Array Intersection
+
+// Given two sorted arrays, `arr1` and `arr2`, return their _intersection_.
+
+// The intersection is a new array that contains all elements that appear in both arrays, in sorted order, including duplicates.
+
+// Example 1:
+// Input:
+// arr1 = [1, 2, 3]
+// arr2 = [1, 3, 5]
+// Output: [1, 3]
+// Explanation: 1 and 3 appear in both arrays.
+
+// Example 2:
+// Input:
+// arr1 = [1, 1, 1]
+// arr2 = [1, 1]
+// Output: [1, 1]
+// Explanation: Two 1s appear in both arrays.
+
+// Example 3:
+// Input:
+// arr1 = [1, 2, 2, 3]
+// arr2 = []
+// Output: []
+
+// Constraints:
+
+// - The input arrays are sorted in ascending order
+// - 0 ≤ arr1.length, arr2.length ≤ 10^6
+// - -10^9 ≤ arr1[i], arr2[i] ≤ 10^9
