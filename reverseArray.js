1c9add995fb0200913c7c9615ba1a711b981008d
Buck <buckm@alumni.stanford.edu>
2026-06-01 18:11:47 -0700

    Description of in-place array reversal problem

---
+++ reverseArray.js
@@ -0,0 +1,21 @@
+// # Array Reversal
+
+// Reverse an array of letters, `arr`, in place using `O(1)` extra space.
+
+// Example 1:
+// Input: arr = ['h', 'e', 'l', 'l', 'o']
+// Output: ['o', 'l', 'l', 'e', 'h']
+
+// Example 2:
+// Input: arr = ['a']
+// Output: ['a']
+
+// Example 3:
+// Input: arr = []
+// Output: []
+
+// Constraints:
+
+// - `0 ≤ arr.length ≤ 10^5`
+// - `arr` contains only letters
+
