35f114c16a5f7ade0e51f92c948e2e7e5c574b44
Buck <buckm@alumni.stanford.edu>
2026-06-18 22:39:22 -0700

    Description of problem
    Added a Ruby script for rotating a square matrix 90 degrees clockwise in place.

---
+++ rotate_clockwise.rb
@@ -0,0 +1,21 @@
+# Matrix Rotation
+
+# Given a square `n x n` matrix, `mat`, rotate it 90 degrees clockwise in place, using `O(1)` extra space.
+
+# Example: mat =  [[25, 15],
+#                  [10, 30]]
+# Output:    mat = [[10, 25],
+#                  [30, 15]]
+# We should not create a new matrix.
+
+# Example: mat = [[1, 2, 3],
+#                 [4, 5, 6],
+#                 [7, 8, 9]]
+# Output:  mat = [[7, 4, 1],
+#                 [8, 5, 2],
+#                 [9, 6, 3]]
+
+# Constraints:
+
+# - `1 <= n <= 1000` where `n` is the size of the square matrix
+# - `-10^9 <= mat[i][j] <= 10^9` (matrix elements are integers)
