8674dd6e589b8dcf10afed59bcf8a476ab98a7be
Buck <buckm@alumni.stanford.edu>
2026-06-23 10:05:07 -0700

    Add findIndex function for substring search

--- findIndex.js
+++ findIndex.js
@@ -14,3 +14,20 @@
 // The length of t is at most 10^5
 // t can be empty, in which case return 0
 // s can be empty, in which case return -1 if t is non-empty, 0 if t is empty
+
+function findIndex(s, t) {
+  if (t.length == 0) return 0;
+  if (s.length == 0) return -1;
+  for (let si = 0; si < s.length - t.length + 1; si++) {
+    if (s[si] === t[0]) {
+      let ti = 1;
+      while (ti < t.length && t[ti] === s[si + ti]) {
+        ti++;
+      }
+      if (ti == t.length) {
+        return si;
+      }
+    }
+  }
+  return -1;
+}
