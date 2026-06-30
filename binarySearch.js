560508a1f50579aec61e1f564ed2a1fce56c38cf
Buck <buckm@alumni.stanford.edu>
2026-06-30 10:51:33 -0700

    Description of simple sorted search array problem

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+// # Search in Sorted Array
+
+// Given a sorted array of integers, `arr`, and a target value, `target`, return the target's index if it exists in the array or `-1` if it doesn't.
+
+// Example 1: arr = [-2, 0, 3, 4, 7, 9, 11], target = 3
+// Output: 2. The target 3 is at index 2.
+
+// Example 2: arr = [-2, 0, 3, 4, 7, 9, 11], target = 2
+// Output: -1. The target 2 is not in the array.
+
+// Example 3: arr = [1, 2, 3], target = 1
+// Output: 0. The target 1 is at index 0.
+
+// Constraints:
+
+// - `0 ≤ arr.length ≤ 10^6`
+// - `-10^9 ≤ arr[i], target ≤ 10^9`
+// - `arr` is sorted in ascending order, without duplicates
